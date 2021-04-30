b21bb36d6aaea67a087d67a466cd18a4380b0aaa
Roman <doroshroman27@example.com>
2021-04-30 16:24:52 +0300

    can compare with other documents now

--- blockchain/deploy.py
+++ blockchain/deploy.py
@@ -1,38 +1,48 @@
 import json
 from web3 import Web3, HTTPProvider
 from web3.contract import ConciseContract
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 w3 = Web3(HTTPProvider("http://127.0.0.1:8545"))
-print(w3.isConnected())
 
-# default account
-private_key = '0x0bb565fc386a4283e484b5cbb750feae0932a0aab577489dc8ca8d792bbee694' # NEED TO CHANGE LATER
-account = w3.eth.account.privateKeyToAccount(private_key)
+def deploy():
 
-# read compiled contract
-truffle_file = json.load(open('./build/contracts/saver.json'))
-abi = truffle_file['abi']
-bytecode = truffle_file['bytecode']
-contract = w3.eth.contract(bytecode=bytecode, abi=abi)
+    account = w3.eth.account.privateKeyToAccount(PRIVATE_KEY)
 
-# build transaction
-build_tx = contract.constructor().buildTransaction({
-    'from': account.address,
-    'nonce': w3.eth.getTransactionCount(account.address),
-    'gas': 1728712,
-    'gasPrice': w3.toWei('21', 'gwei')
-})
+    # read compiled contract
+    truffle_file = json.load(open(os.path.join(basedir, './build/contracts/saver.json')))
+    abi = truffle_file['abi']
+    bytecode = truffle_file['bytecode']
+    contract = w3.eth.contract(bytecode=bytecode, abi=abi)
 
-signed = account.sign_transaction(build_tx)
-tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
+    # build transaction
+    build_tx = contract.constructor().buildTransaction({
+        'from': account.address,
+        'nonce': w3.eth.getTransactionCount(account.address),
+        'gas': 1728712,
+        'gasPrice': w3.toWei('21', 'gwei')
+    })
 
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-contract_address = tx_receipt['contractAddress']
+    signed = account.sign_transaction(build_tx)
+    tx_hash = w3.eth.send_raw_transaction(signed.rawTransaction)
 
-# dump contract address and abi
-with open('contract.json', 'w') as out:
-    data = {
-        "abi": abi,
-        "contract_address": contract_address
-    }
-    json.dump(data, out, indent=4, sort_keys=True)
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    contract_address = tx_receipt['contractAddress']
+
+    # dump contract address and abi
+    with open('contract.json', 'w') as out:
+        data = {
+            "abi": abi,
+            "contract_address": contract_address
+        }
+        json.dump(data, out, indent=4, sort_keys=True)
+    
+if __name__ == '__main__':
+    deploy()
